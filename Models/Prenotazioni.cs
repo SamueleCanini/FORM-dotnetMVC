b9ee5cca0dbf2e8f7ccc3846f5c8f68a5d5153c1
Samuele Canini <116788363+SamueleCanini@users.noreply.github.com>
2024-01-20 12:47:19 +0100

    Add files via upload

---
+++ Models/Prenotazioni.cs
@@ -0,0 +1,23 @@
+public class Prenotazioni{
+
+    public string? Nome { get; set; } required
+    public string? Cognome { get; set; }required
+    public string? Email { get; set; }required
+    public  DateTime DataDiNascita { get; set; }required
+    public Sesso sesso { get; set; }
+    public Ruolo ruolo { get; set; }
+
+    public enum Sesso{
+        M,
+        F,
+        NoSpecificato,
+    }
+
+    public enum Ruolo{
+        Insegnante,
+        Genitore,
+        Studente,
+    }
+
+
+}
